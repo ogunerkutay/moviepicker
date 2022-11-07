29eae8b01463c292bfe61a3357de99fb6f61db8d
OGÜN ERKUTAY <77350593+ogunerkutay@users.noreply.github.com>
2022-11-07 13:26:52 +0300

    second

--- moviepicker.py
+++ moviepicker.py
@@ -10,22 +10,16 @@ data = pd.read_csv('ratings.csv')
 data.head(10)
 
 movie_titles_genre = pd.read_csv("movies.csv")
-movie_titles_genre.head(10)
 
 data = data.merge(movie_titles_genre,on='movieId', how='left')
-data.head(10)
 
 Average_ratings = pd.DataFrame(data.groupby('title')['rating'].mean())
-Average_ratings.head(10)
 
 Average_ratings['Total Ratings'] = pd.DataFrame(data.groupby('title')['rating'].count())
-Average_ratings.head(10)
 
 movie_user = data.pivot_table(index='userId',columns='title',values= 'rating')
-movie_user.head(10)
 
 correlations = movie_user.corrwith(movie_user['Toy Story (1995)'])
-correlations.head()
 
 recommendation = pd.DataFrame(correlations,columns=['Correlation'])
 recommendation.dropna(inplace=True)
@@ -34,5 +28,8 @@ recommendation.head()
 
 recc = recommendation[recommendation['Total Ratings']>100].sort_values('Correlation',ascending=False).reset_index()
 recc = recc.merge(movie_titles_genre,on='title', how='left')
-recc.head(10)
 
+
+text_input = st.text_input("Enter a movie name","Toy Story (1995)")
+
+st.markdown(f"Tavsieler : {recc.head(10)}");
